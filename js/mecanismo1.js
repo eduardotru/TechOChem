3332ebe57bb1a03f90b919b959a230516d071ffc
Eduardo Trujillo <edytr1997@gmail.com>
2018-11-14 12:39:16 -0600

    Sample fro creating molecules and bonds

--- js/mecanismo1.js
+++ js/mecanismo1.js
@@ -1,108 +1,140 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 
-// globals
-var curveLayer, lineLayer, anchorLayer, quad;
-
-function updateDottedLines() {
-    var q = quad;
-
-    var quadLine = lineLayer.get('#quadLine')[0];
-
-    quadLine.setPoints([q.start.attrs.x, q.start.attrs.y, q.control.attrs.x, q.control.attrs.y, q.end.attrs.x, q.end.attrs.y]);
-
-    lineLayer.draw();
-}
-
-function buildAnchor(x, y) {
-    var anchor = new Konva.Circle({
-        x: x,
-        y: y,
-        radius: 20,
-        stroke: '#666',
-        fill: '#ddd',
-        strokeWidth: 2,
-        draggable: true
-    });
-
-    // add hover styling
-    anchor.on('mouseover', function() {
-        document.body.style.cursor = 'pointer';
-        this.setStrokeWidth(4);
-        anchorLayer.draw();
-    });
-    anchor.on('mouseout', function() {
-        document.body.style.cursor = 'default';
-        this.setStrokeWidth(2);
-        anchorLayer.draw();
-
-    });
-
-    anchor.on('dragend', function() {
-        drawCurves();
-        updateDottedLines();
-    });
-
-    anchorLayer.add(anchor);
-    return anchor;
-}
-function drawCurves() {
-    var context = curveLayer.getContext();
-
-    context.clear();
-
-    // draw quad
-    context.beginPath();
-    context.moveTo(quad.start.attrs.x, quad.start.attrs.y);
-    context.quadraticCurveTo(quad.control.attrs.x, quad.control.attrs.y, quad.end.attrs.x, quad.end.attrs.y);
-    context.setAttr('strokeStyle', 'red');
-    context.setAttr('lineWidth', 4);
-    context.stroke();
-}
-
 var stage = new Konva.Stage({
-    container: 'canvas',
-    width: width-50,
-    height: height-200
-});
-
-anchorLayer = new Konva.Layer();
-lineLayer = new Konva.Layer();
-
-// curveLayer just contains a canvas which is drawn
-// onto with the existing canvas API
-curveLayer = new Konva.Layer();
-
-var quadLine = new Konva.Line({
-    dash: [10, 10, 0, 10],
-    strokeWidth: 3,
-    stroke: 'black',
-    lineCap: 'round',
-    id: 'quadLine',
-    opacity: 0.3,
-    points: [0, 0]
-});
-
-// add dotted line connectors
-lineLayer.add(quadLine);
-
-quad = {
-    start: buildAnchor(60, 30),
-    control: buildAnchor(240, 110),
-    end: buildAnchor(80, 160)
-};
-
-// keep curves insync with the lines
-anchorLayer.on('beforeDraw', function() {
-    drawCurves();
-    updateDottedLines();
+  container: 'canvas',
+  width: width,
+  height: height
 });
 
 
+var layer = new Konva.Layer();
 
-stage.add(curveLayer);
-stage.add(lineLayer);
-stage.add(anchorLayer);
 
-drawCurves();
-updateDottedLines();
+// Generic function to draw an atom with a letter
+function atom(x, y, draggable, color, stroke, radius, name, textColor) {
+  let group = new Konva.Group({
+    draggable: draggable
+  });
+
+  let t = new Konva.Text({
+    x: x-11,
+    y: y-11,
+    text: name,
+    fontSize: 30,
+    fill: textColor,
+  });
+
+  let a = new Konva.Circle({
+    x: x,
+    y: y,
+    radius: radius,
+    fill: color,
+    stroke: stroke,
+    strokeWidth: 4,
+  });
+
+  group.add(a);
+  group.add(t);
+  return group;
+}
+
+// Atom that can be created.
+function carbon(x, y, draggable) {
+  return atom(x, y, draggable, '#333', '#000', 5*4 + 20, 'C', '#fff');
+}
+
+function oxygen(x, y, draggable) {
+  return atom(x, y, draggable, '#f00', '#a00', 7*4 + 20, 'O', '#000');
+}
+
+function hydrogen(x, y, draggable) {
+  return atom(x, y, draggable, '#999', '#777', 1*4 + 20, 'H', '#000');
+}
+
+// Bonds between atoms
+function singleBond(atom1, atom2) {
+  return new Konva.Line({
+    points: [atom1.children[0].attrs.x, atom1.children[0].attrs.y, atom2.children[0].attrs.x, atom2.children[0].attrs.y],
+    stroke: '#000',
+    strokeWidth: 8,
+    lineCap: 'round'
+  });
+}
+
+function doubleBond(atom1, atom2) {
+  let g = new Konva.Group({
+    draggable: false
+  });
+  let line1 = singleBond(atom1, atom2);
+  let line2 = singleBond(atom1, atom2);
+  line1.move({
+    x: -8,
+    y: -8,
+  });
+
+  line2.move({
+    x: 8,
+    y: 8
+  });
+
+  g.add(line1);
+  g.add(line2);
+  return g;
+}
+
+// Actions in events
+function destroy(e) {
+  e.target.destroy();
+  layer.draw();
+}
+
+let hydrogen1 = hydrogen(100, 200, false);
+let hydrogen2 = hydrogen(300, 200, false);
+let hydrogen3 = hydrogen(200, 100, false);
+let hydrogen4 = hydrogen(200, 300, false);
+let carbon1 = carbon(200, 200, false);
+
+let ch4 = new Konva.Group({
+  draggable: true
+});
+
+let bond1 = singleBond(hydrogen1, carbon1);
+let bond2 = singleBond(hydrogen2, carbon1);
+let bond3 = singleBond(hydrogen3, carbon1);
+let bond4 = singleBond(hydrogen4, carbon1);
+
+bond1.on('click', destroy);
+
+ch4.add(bond1);
+ch4.add(hydrogen1);
+ch4.add(bond2);
+ch4.add(hydrogen2);
+ch4.add(bond2);
+ch4.add(hydrogen2);
+ch4.add(bond3);
+ch4.add(hydrogen3);
+ch4.add(bond4);
+ch4.add(hydrogen4);
+ch4.add(carbon1);
+
+let oxygen1 = oxygen(400, 100, false);
+let oxygen2 = oxygen(700, 100, false);
+let carbon2 = carbon(550, 100, false);
+
+let doubleBond1 = doubleBond(oxygen1, carbon2);
+let doubleBond2 = doubleBond(oxygen2, carbon2);
+
+let co2 = new Konva.Group({
+  draggable: true
+});
+
+co2.add(doubleBond1);
+co2.add(doubleBond2);
+co2.add(oxygen1);
+co2.add(oxygen2);
+co2.add(carbon2);
+
+layer.add(co2);
+layer.add(ch4);
+stage.add(layer);
