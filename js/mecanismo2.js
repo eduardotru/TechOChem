15e29fc5dfaec3951b963fc3c2fba5720352ce9e
Angel Seiji Morimoto Burgos <angel_seiji@hotmail.com>
2018-11-15 19:08:58 -0600

    - Simplified data to include in JSON files. - Added reusable functions in atoms.js - Added use of export/import.

--- js/mecanismo2.js
+++ js/mecanismo2.js
@@ -1,3 +1,5 @@
+import { createAtomsInLayer, createBondsInLayer } from './atoms.js';
+
 let stageWidth = 1000;
 let stageHeight = 600;
 
@@ -22,40 +24,6 @@ function fitStageIntoParentContainer() {
   stage.draw();
 }
 
-function createSingleBondInLayer(atomId1, atomId2, layer) {
-  let atom1 = layer.findOne(`#${atomId1}`);
-  let atom2 = layer.findOne(`#${atomId2}`);
-  let bond = new Konva.Line({
-    points: [atom1.children[0].x(), atom1.children[0].y(),
-             atom2.children[0].x(), atom2.children[0].y()],
-    stroke: '#000',
-    strokeWidth: 8
-  });
-  layer.add(bond);
-  bond.moveToBottom();
-}
-
-function createBondInLayer(bondInfo, layer) {
-  switch(bondInfo.type) {
-    case "single":
-      createSingleBondInLayer(bondInfo.atom1, bondInfo.atom2, layer);
-      break;
-  }
-}
-
-function createBondsInLayer(bondsInfo, layer) {
-  bondsInfo.map((bondInfo) => {
-    createBondInLayer(bondInfo, layer);
-  });
-}
-
-function createAtomsInLayer(atomsInfo, layer) {
-  atomsInfo.map((atom) => {
-    let node = Konva.Node.create(atom);
-    layer.add(node);
-  });
-}
-
 $.ajax({
   url: './data/mecanismo2.json',
   type: 'GET',
@@ -71,4 +39,3 @@ $.ajax({
     console.log(err);
   }
 });
-
